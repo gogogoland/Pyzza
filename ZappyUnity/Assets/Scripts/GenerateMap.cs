9bd81e5a191c5fea50119e3b80b28d47b4fd8a8b
NicoDGR <nd-heyge@student.42.fr>
2016-05-10 20:19:21 +0200

    PerlinNoise au revoir tu sers a rien + gestion ressources et bouffe aleatoire geree

--- ZappyUnity/Assets/Scripts/GenerateMap.cs
+++ ZappyUnity/Assets/Scripts/GenerateMap.cs
@@ -8,50 +8,98 @@ public class GenerateMap : MonoBehaviour {
 	public int					width;
 	public int					seed;
 	public GameObject			tile;
+	public GameObject			food_obj;
+	public GameObject			ressources_obj;
+	public Sprite				[]ressources_sprite;
 	public Material				[]materials;
 
-	private Noise				noise;
 	private Material			[,]variant_materials;
+	private GameObject			[,]variant_ressources;
+
 	private GameObject			[,]tiles;
 	// Use this for initialization
 	void Start () {
 		Test();
 		Generate(height, width);
-		CenterCamera(height);
 	}
 
-	void Test() {
+	int		TestRessourcesRandom() {
+		float rnd = Random.value;
+
+		if (rnd < 0.1f)
+			return (6);
+		else if (rnd < 0.30f)
+		{
+			float rnd2 = Random.value;
+			if (rnd2 < 0.50f)
+				return (4);
+			return (5);
+		}
+		else if (rnd < 0.50f)
+		{
+			float rnd2 = Random.value;
+			if (rnd2 < 0.33f)
+				return (3);
+			else if (rnd2 < 0.66f)
+				return (2);
+			return (1);
+		}
+		return (0);
+	}
+
+	void	TestAssignResrc(float rnd, int tileX, int tileY) {
+		Vector3		vec = Vector3.zero;
+
+		if (rnd < 0.15f)
+		{
+			vec.x = tileX * tile.transform.localScale.x * 10;
+			vec.y = 1.4f;
+			vec.z = tileY * tile.transform.localScale.z * 10;
+			if (variant_ressources[tileX, tileY] == null) {
+				variant_ressources[tileX, tileY] = GameObject.Instantiate(food_obj, vec, food_obj.transform.rotation) as GameObject;
+				variant_ressources[tileX, tileY].GetComponent<SpriteRenderer>().sprite = ressources_sprite[0];
+			}
+		}
+		else if (rnd < 0.50f) {
+			vec.x = tileX * tile.transform.localScale.x * 10;
+			vec.y = 3.0f;
+			vec.z = tileY * tile.transform.localScale.z * 10;
+			int rd = TestRessourcesRandom();
+			if (rd != 0 && variant_ressources[tileX, tileY] == null)
+			{
+				variant_ressources[tileX, tileY] = GameObject.Instantiate(ressources_obj, vec, ressources_obj.transform.rotation) as GameObject;
+				variant_ressources[tileX, tileY].GetComponent<SpriteRenderer>().sprite = ressources_sprite[rd];
+			}
+		}
+
+	}
+
+	void	TestAssignTile(float rnd, int tileX, int tileY) {
+		if (rnd < 0.25f)
+			variant_materials[tileX, tileY] = materials[2];
+		else if (rnd < 0.50f)
+			variant_materials[tileX, tileY] = materials[1];
+		else if (rnd < 1.0f)
+			variant_materials[tileX, tileY] = materials[0];
+	}
+
+	void	Test() {
 		variant_materials = new Material[width, height];
-		noise = new Noise(0);
-		float r_noise;
+		variant_ressources = new GameObject[width, height];
 
 		Random.seed = seed;
 		for (int tileX = 0; tileX < width; tileX++)
 		{
 			for (int tileY = 0; tileY < height; tileY++)
 			{
-				r_noise = noise.FractalNoise2D(width, height, 3, 5, 0.1f) + 0.1f;
-				float rnd = Random.value;
-				if (r_noise > 0.0) {
-					if (rnd < 0.25f)
-						variant_materials[tileX, tileY] = materials[2];
-					else if (rnd < 0.50f)
-						variant_materials[tileX, tileY] = materials[1];
-					else if (rnd < 1.0f)
-						variant_materials[tileX, tileY] = materials[0];
-				}
+				TestAssignResrc(Random.value, tileX, tileY);
+				TestAssignTile(Random.value, tileX, tileY);
 			}
 		}
 	}
 
-	void CenterCamera(int h) {
-		float x = (h * tile.transform.localScale.x * 10 / 2 ) - (tile.transform.localScale.x * 10 / 2);
-		GameObject cam = GameObject.Find("Main Camera");
 
-		cam.transform.position = new Vector3(x, 10, 0);
-	}
-
-	void Generate(int h, int w) {
+	void	Generate(int h, int w) {
 		Vector3		vec = Vector3.zero;
 		tiles = new GameObject[width, height];
 
@@ -69,9 +117,9 @@ public class GenerateMap : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update ()
+	void	Update ()
 	{
-		Test();
+//		Test();
 		for (int x = 0; x < width; x++)
 		{
 			for (int z = 0; z < height; z++)
---
+++ ZappyUnity/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	public float		speed;
+	private GenerateMap	scriptmap;
+
+	
+	void	CenterCamera(int w) {
+		float x = (w * scriptmap.tile.transform.localScale.x * 10 / 2 ) - (scriptmap.tile.transform.localScale.x * 10 / 2);
+		transform.position = new Vector3(x, 15, 0);
+	}
+
+	// Use this for initialization
+	void Start () {
+		scriptmap = GameObject.Find("GenerateMap").GetComponent<GenerateMap>();
+		CenterCamera(scriptmap.width);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+			transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+			transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+			transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
+		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+			transform.Translate(Vector3.back * Time.deltaTime * speed, Space.World);
+	}
+}
