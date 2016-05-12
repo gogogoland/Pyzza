aafb7c6e8ae43ee6404fd79108defea511b9fe5b
NicoDGR <nd-heyge@student.42.fr>
2016-05-12 19:41:31 +0200

    taille de map rectangulaire geree, limiter a 11*11 + debut de connection

--- ZappyUnity/Assets/Scripts/GenerateMap.cs
+++ ZappyUnity/Assets/Scripts/GenerateMap.cs
@@ -18,125 +18,75 @@ public class GenerateMap : MonoBehaviour {
 
 	private GameObject			[,]tiles;
 	private GameObject			map;
-	private GameObject			tilesParent;
-	private GameObject			tilesParentCpy;
+	private GameObject			[]border;
+	private GameObject			lineTmp;
+	private GameObject			[]lines;
+
+	private int					nbrBorder = 0;
 
 	// Use this for initialization
-	void Start () {
-		map = new GameObject("Map");
-		tilesParent = new GameObject("Tiles");
-		tilesParentCpy = new GameObject("Tiles");
-		tilesParent.transform.parent = map.transform;
+	void Start ()
+	{
+		InitMap();
 		Test();
 		Generate(height, width);
 		Test2();
-		GenerateDbl();
-//		Debug.Log (7 * 10 * tile.transform.position);
-		Destroy(tilesParentCpy);
+		if (height >= 11 && width >= 11)
+			GenerateBorder();
+		Destroy(lineTmp);
+
 	}
 
-	void	Sector5()
+	void InitMap()
 	{
-		GameObject	map = new GameObject("Sector5");
-		map.transform.position = new Vector3(width * 10, 0, 0);
-		GameObject tilesCpy = GameObject.Instantiate(tilesParentCpy, map.transform.position, Quaternion.identity) as GameObject;
-		tilesCpy.transform.parent = map.transform;
-		for (int x = 0; x < 8; x++)
-		{
-			for (int z = 0; z < width; z++)
-			{
-				GameObject tile_current = tilesParent.transform.GetChild(z + width * x).gameObject;
-				GameObject tile_clone = GameObject.Instantiate(tile_current,
-				                                               tile_current.transform.localPosition + map.transform.position,
-				                                               Quaternion.identity) as GameObject;
-				tile_clone.transform.parent = tilesCpy.transform;
-			}
+		map = new GameObject("Map");
+		lineTmp = new GameObject("Lines");
+		lines = new GameObject[height];
+		border = new GameObject[5];
+		for (int z = height - 1; z > -1; z--) {
+			lines[z] = GameObject.Instantiate(lineTmp, map.transform.position, Quaternion.identity) as GameObject;
+			lines[z].transform.parent = map.transform;
+			lines[z].name = "Lines (" + (map.transform.childCount - 1) + ")";
 		}
 	}
 
-	void	Sector4()
+	void	Border(Vector3 position, int initX, int initZ, int limitX, int limitZ)
 	{
-		GameObject	map = new GameObject("Sector4");
-		map.transform.position = new Vector3(width * -10, 0, 0);
-		GameObject tilesCpy = GameObject.Instantiate(tilesParentCpy, map.transform.position, Quaternion.identity) as GameObject;
-		tilesCpy.transform.parent = map.transform;
-		for (int x = height - 8; x < height; x++)
+		border[nbrBorder] = new GameObject("Border" + nbrBorder);
+		border[nbrBorder].transform.position = position;
+		for (int z = initZ; z < limitZ; z++)
 		{
-			for (int z = 0; z < width; z++)
+			GameObject lineCpy = GameObject.Instantiate(lineTmp, border[nbrBorder].transform.position, Quaternion.identity) as GameObject;
+			lineCpy.transform.parent = border[nbrBorder].transform;
+			lineCpy.name = "Lines (" + z + ")";
+			for (int x = initX; x < limitX; x++)
 			{
-				GameObject tile_current = tilesParent.transform.GetChild(z + width * x).gameObject;
+				GameObject tile_current = map.transform.GetChild (z).transform.GetChild(x).gameObject;
 				GameObject tile_clone = GameObject.Instantiate(tile_current,
-				                                               tile_current.transform.localPosition + map.transform.position,
+				                                               tile_current.transform.localPosition + border[nbrBorder].transform.position,
 				                                               Quaternion.identity) as GameObject;
-				tile_clone.transform.parent = tilesCpy.transform;
+				tile_clone.name = "Tile(" + z + ", " + x + ")";
+				tile_clone.transform.parent = lineCpy.transform;
 			}
 		}
+		nbrBorder++;
 	}
 
-	void	Sector3()
+	void GenerateBorder()
 	{
-		GameObject	map = new GameObject("Sector3");
-		map.transform.position = new Vector3(width * 10, 0, height * 10);
-		GameObject tilesCpy = GameObject.Instantiate(tilesParentCpy, map.transform.position, Quaternion.identity) as GameObject;
-		tilesCpy.transform.parent = map.transform;
-		for (int x = 0; x < 8; x++)
-		{
-			for (int z = 0; z < 11; z++)
-			{
-				GameObject tile_current = tilesParent.transform.GetChild(z + width * x).gameObject;
-				GameObject tile_clone = GameObject.Instantiate(tile_current,
-				                                               tile_current.transform.localPosition + map.transform.position,
-				                                               Quaternion.identity) as GameObject;
-				tile_clone.transform.parent = tilesCpy.transform;
-			}
-		}
-	}
+		Vector3		posBorder1 = new Vector3(width * -10, 0, height * 10);
+		Vector3		posBorder2 = new Vector3(0, 0, height * 10);
+		Vector3		posBorder3 = new Vector3(width * 10, 0, height * 10);
+		Vector3		posBorder4 = new Vector3(width * -10, 0, 0);
+		Vector3		posBorder5 = new Vector3(width * 10, 0, 0);
 
-	void	Sector2()
-	{
-		GameObject	map = new GameObject("Sector2");
-		map.transform.position = new Vector3(0, 0, height * 10);
-		GameObject tilesCpy = GameObject.Instantiate(tilesParentCpy, map.transform.position, Quaternion.identity) as GameObject;
-		tilesCpy.transform.parent = map.transform;
-		for (int x = 0; x < height; x++)
-		{
-			for (int z = 0; z < 11; z++)
-			{
-				GameObject tile_current = tilesParent.transform.GetChild(z + width * x).gameObject;
-				GameObject tile_clone = GameObject.Instantiate(tile_current,
-				                                               tile_current.transform.localPosition + map.transform.position,
-				                                               Quaternion.identity) as GameObject;
-				tile_clone.transform.parent = tilesCpy.transform;
-			}
-		}
-	}
-
-	void	Sector1()
-	{
-		GameObject	map = new GameObject("Sector1");
-		map.transform.position = new Vector3(width * -10, 0, height * 10);
-		GameObject tilesCpy = GameObject.Instantiate(tilesParentCpy, map.transform.position, Quaternion.identity) as GameObject;
-		tilesCpy.transform.parent = map.transform;
-		for (int x = height - 8; x < height; x++)
-		{
-			for (int z = 0; z < 11; z++)
-			{
-				GameObject tile_current = tilesParent.transform.GetChild(z + width * x).gameObject;
-				GameObject tile_clone = GameObject.Instantiate(tile_current,
-				                                               tile_current.transform.localPosition + map.transform.position,
-				                                               Quaternion.identity) as GameObject;
-				tile_clone.transform.parent = tilesCpy.transform;
-			}
-		}
-	}
-
-	void GenerateDbl()
-	{
-		Sector1();
-		Sector2();
-		Sector3();
-		Sector4();
-		Sector5();
+		Border(posBorder1, width - 8, height - 11, width, height);
+		Border(posBorder2, 0, height - 11, width, height);
+		Border(posBorder3, 0, height - 11, 8, height);
+		Border(posBorder4, width - 8, 0, width, height);
+		Border(posBorder5, 0, 0, 8, height);
+		for (int b = 0; b < border.Length; b++)
+			border[b].AddComponent<RefreshBorderMap>();
 	}
 
 	int		TestRessourcesRandom() {
@@ -171,10 +121,10 @@ public class GenerateMap : MonoBehaviour {
 			vec.x = tileX * tile.transform.localScale.x * 10;
 			vec.y = 1.4f;
 			vec.z = tileY * tile.transform.localScale.z * 10;
-			if (variant_ressources[tileX, tileY] == null) {
-				variant_ressources[tileX, tileY] = GameObject.Instantiate(food_obj, vec, food_obj.transform.rotation) as GameObject;
-				variant_ressources[tileX, tileY].transform.parent = tiles[tileX, tileY] .transform;
-				variant_ressources[tileX, tileY].GetComponent<SpriteRenderer>().sprite = ressources_sprite[0];
+			if (variant_ressources[tileY, tileX] == null) {
+				variant_ressources[tileY, tileX] = GameObject.Instantiate(food_obj, vec, food_obj.transform.rotation) as GameObject;
+				variant_ressources[tileY, tileX].transform.parent = tiles[tileY, tileX] .transform;
+				variant_ressources[tileY, tileX].GetComponent<SpriteRenderer>().sprite = ressources_sprite[0];
 			}
 		}
 		else if (rnd < 0.50f) {
@@ -182,11 +132,11 @@ public class GenerateMap : MonoBehaviour {
 			vec.y = 3.0f;
 			vec.z = tileY * tile.transform.localScale.z * 10;
 			int rd = TestRessourcesRandom();
-			if (rd != 0 && variant_ressources[tileX, tileY] == null)
+			if (rd != 0 && variant_ressources[tileY, tileX] == null)
 			{
-				variant_ressources[tileX, tileY] = GameObject.Instantiate(ressources_obj, vec, ressources_obj.transform.rotation) as GameObject;
-				variant_ressources[tileX, tileY].transform.parent = tiles[tileX, tileY].transform;
-				variant_ressources[tileX, tileY].GetComponent<SpriteRenderer>().sprite = ressources_sprite[rd];
+				variant_ressources[tileY, tileX] = GameObject.Instantiate(ressources_obj, vec, ressources_obj.transform.rotation) as GameObject;
+				variant_ressources[tileY, tileX].transform.parent = tiles[tileY, tileX].transform;
+				variant_ressources[tileY, tileX].GetComponent<SpriteRenderer>().sprite = ressources_sprite[rd];
 			}
 		}
 
@@ -194,15 +144,15 @@ public class GenerateMap : MonoBehaviour {
 
 	void	TestAssignTile(float rnd, int tileX, int tileY) {
 		if (rnd < 0.25f)
-			variant_materials[tileX, tileY] = materials[2];
+			variant_materials[tileY, tileX] = materials[2];
 		else if (rnd < 0.50f)
-			variant_materials[tileX, tileY] = materials[1];
+			variant_materials[tileY, tileX] = materials[1];
 		else if (rnd < 1.0f)
-			variant_materials[tileX, tileY] = materials[0];
+			variant_materials[tileY, tileX] = materials[0];
 	}
 
 	void	Test() {
-		variant_materials = new Material[width, height];
+		variant_materials = new Material[height, width];
 
 		Random.seed = seed;
 		for (int tileX = 0; tileX < width; tileX++)
@@ -211,9 +161,9 @@ public class GenerateMap : MonoBehaviour {
 				TestAssignTile(Random.value, tileX, tileY);
 		}
 	}
-	
+
 	void	Test2() {
-		variant_ressources = new GameObject[width, height];
+		variant_ressources = new GameObject[height, width];
 		
 		Random.seed = seed;
 		for (int tileX = 0; tileX < width; tileX++)
@@ -225,18 +175,21 @@ public class GenerateMap : MonoBehaviour {
 
 	void	Generate(int h, int w) {
 		Vector3		vec = Vector3.zero;
-		tiles = new GameObject[width, height];
+		tiles = new GameObject[height, width];
 
-		for (int x = 0; x < w; x++)
+		for (int z = height - 1; z > -1; z--)
 		{
-			for (int z = 0; z < h; z++)
+			int j = 0;
+			for (int x = 0; x < width; x++)
 			{
 				vec.x = x * tile.transform.localScale.x * 10;
 				vec.z = z * tile.transform.localScale.z * 10;
-				tiles[x, z] = GameObject.Instantiate(tile, vec, tile.transform.rotation) as GameObject;
-				tiles[x, z].transform.parent = tilesParent.transform;
-				tiles[x, z].GetComponent<Renderer>().material = variant_materials[x, z];
+				tiles[z, x] = GameObject.Instantiate(tile, vec, tile.transform.rotation) as GameObject;
+				tiles[z, x].transform.parent = lines[z].transform;
+				tiles[z, x].transform.name = "Tile(" + j + ", " + x + ")";
+				tiles[z, x].GetComponent<Renderer>().material = variant_materials[z, x];
 			}
+			j++;
 		}
 
 	}
---
+++ ZappyUnity/Assets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	private InputField		inputIP;
+	private InputField		inputPort;
+	private Text			error;
+
+	private int				ip = 0;
+	void Start () {
+		inputIP = GameObject.Find("InputFieldIP").GetComponent<InputField>();
+		inputPort = GameObject.Find("InputFieldPort").GetComponent<InputField>();
+		error = GameObject.Find("Error").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inputPort.text != "")
+			int.TryParse(inputPort.text, out ip);
+//			
+	}
+
+	public void Connect() {
+		NetworkConnectionError e;
+
+		int.TryParse(inputPort.text, out ip);
+		if (ip != 0 && inputIP.text != "")
+		{
+			e = Network.Connect(inputIP.text, ip);
+			if (string.Compare(e.ToString(), "NoError") != 0)
+				error.text = "Error : " + e;
+		}
+		else
+			error.text = "Error : Missing Ip or Port";
+		//		if (string.Compare((e = Network.Connect(inputIP.text, int.Parse (inputPort.text)))), "NoError")
+//			error.text = "Error : " + e;
+
+	}
+}
