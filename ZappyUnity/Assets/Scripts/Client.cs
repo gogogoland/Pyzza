e3d077c900da4cbf7a327b2f2b093e59881dcbb6
NicoDGR <nd-heyge@student.42.fr>
2016-05-13 23:11:02 +0200

    reseau fini + gestion d'erreur

---
+++ ZappyUnity/Assets/Scripts/Client.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Net.Sockets;
+using System;
+
+public class Client : MonoBehaviour {
+
+	public string			inputIP;
+	public int				ip;
+	public Text			error;
+
+	private TcpClient		client;
+	private NetworkStream	serverStream;
+
+	// Use this for initialization
+
+	IEnumerator	LaunchGame() {
+		error.text = "Success !";
+		error.color = Color.green;
+		DontDestroyOnLoad(gameObject);
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel("Game");
+	}
+
+	void Start () {
+		error = GameObject.Find("Error").GetComponent<Text>();
+		client = new TcpClient();
+		try {
+			client.Connect(inputIP, ip);
+			StartCoroutine(LaunchGame());
+		}
+		catch (Exception e) 
+		{
+			error.text = "Error : " + e;
+			error.color = Color.red;
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- ZappyUnity/Assets/Scripts/Menu.cs
+++ ZappyUnity/Assets/Scripts/Menu.cs
@@ -1,51 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-using System.Net.Sockets;
-using System;
 
 public class Menu : MonoBehaviour {
 
 	// Use this for initialization
+	public GameObject				client;
+
 	private InputField				inputIP;
 	private InputField				inputPort;
-	private Text					error;
-	private TcpClient				client;
-	private NetworkStream			serverStream;
-
-	private int						ip = 0;
+	private GameObject				clientclone;
 
 	void Start () {
 		inputIP = GameObject.Find("InputFieldIP").GetComponent<InputField>();
 		inputPort = GameObject.Find("InputFieldPort").GetComponent<InputField>();
-		error = GameObject.Find("Error").GetComponent<Text>();
-		client = new TcpClient();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-	}
-
-	IEnumerator	LaunchGame() {
-		error.text = "Success !";
-		error.color = Color.green;
-		yield return new WaitForSeconds(3);
-		Application.LoadLevel("Game");
 	}
 
 	public void Connect() {
-		error.text = "Loading ...";
-		error.color = Color.black;
-		int.TryParse(inputPort.text, out ip);
-		try {
-			client.Connect(inputIP.text, ip);
-			StartCoroutine(LaunchGame());
-		}
-		catch (Exception e) 
-		{
-			error.text = "Error : " + e;
-			error.color = Color.red;
-		}
+		clientclone = GameObject.Instantiate(client, Vector3.zero, Quaternion.identity) as GameObject;
+		clientclone.GetComponent<Client>().inputIP = inputIP.text;
+		int.TryParse(inputPort.text, out clientclone.GetComponent<Client>().ip);
+
 	}
 }
